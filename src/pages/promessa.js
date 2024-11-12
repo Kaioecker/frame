32d1b00d184bc8bbbe559ac913674633cdfd7f0a
Kaioecker <kaio.ecker@estudante.ifms.edu.br>
2024-11-12 14:13:38 -0300

    Add files via upload

--- src/pages/promessa.js
+++ src/pages/promessa.js
@@ -1,19 +1,27 @@
 import { Container } from "react-bootstrap";
 import Footer from "./components/footer";
 import Menu from "./components/navbar";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Promessa() {
     const [divida, setDivida] = useState("")
-    setTimeout(() => {
-        document.title = "Minha Promessa"
-        setDivida("da vida")
-    }, 4000);
+    useEffect(() => {
+        setTimeout(async () => {
+            document.title = "Minha Promessa"
+            const x = await saoLonguinho("da vida")
+            setDivida(x)
+        }, 4000);
+    })
     return <>
-        <Menu />
-        <Container>
-            Promessas {(divida==="" ? "<carregando>" : divida)}
-        </Container>
-        <Footer />
+        <Menu /> <Container>
+            Promessas {(divida === "" ? <img src="loading_3.gif" width={40} height={60} />
+                : divida)}
+        </Container>  <Footer />
     </>
+}
+function saoLonguinho(texto) {
+    return new Promise((resposta) => 
+        setTimeout(() => {
+        resposta(texto)
+    }, 4000))
 }
